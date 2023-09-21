42908de8b8b6f220bcbccdfe0126d7e5afe356f4
dykim <onurleft@naver.com>
2023-09-21 17:28:18 +0900

    [ADD] 230921

--- twitter/views.py
+++ twitter/views.py
@@ -3,6 +3,7 @@ from .models import Twit
 from django.utils import timezone
 from .forms import TwitForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def index(request):
     twits = Twit.objects.order_by('-create_date')
@@ -15,10 +16,7 @@ def detail(request, twit_id):
 
     children = thistwit.get_all_children()
     for child in children:
-        thread.append({'id': child.id, 'parent_id': child.parent_id, 'content': child.content})
-        
-
-
+        thread.append({'id': child.id, 'parent_id': child.parent_id, 'content': child.content, 'author': child.author.username, 'create_date': child.create_date})
     
     context = {'thread': thread, 'thistwit': thistwit}
     return render(request, 'twitter/twit_detail.html', context)
@@ -42,4 +40,9 @@ def twit_create(request):
 @login_required(login_url='common:login')
 def twit_delete(request, twit_id):
     twit = get_object_or_404(Twit, pk=twit_id)
+    if request.user != twit.author:
+        messages.error(request, '삭제 권한이 없습니다.')
+        return redirect('twit:detail', twit_id=twit.id)
+    twit.delete()
+    return redirect('twitter:index')
     
